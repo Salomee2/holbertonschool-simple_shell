31c1a1dadc72abd0c20414d7fabdc8a94fa3836b
root <pertonsalomee@gmail.com>
2025-01-09 18:44:40 +0000

    trying to fix task4

--- main.c
+++ main.c
@@ -1,66 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <sys/wait.h>
 #include "shell.h"
 
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    pid_t pid;
-    char *args[2];
+	char *line = NULL;
+	size_t len = 0;
+	pid_t pid;
+	char *args[2];
+	char *cmd_path;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            printf("$ ");
+	while (1)
+	{
+		printf("$ ");
+		if (getline(&line, &len, stdin) == -1)
+		{
+			printf("\n");
+			break;
+		}
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            if (feof(stdin))
-            {
-                printf("\n");
-                free(line);
-                exit(0);
-            }
-            perror("getline");
-            free(line);
-            exit(1);
-        }
+		line[strcspn(line, "\n")] = '\0';
 
-        line[strcspn(line, "\n")] = '\0';
+		if (strlen(line) == 0)
+			continue;
 
-        if (strlen(line) == 0)
-            continue;
+		cmd_path = find_command_in_path(line);
 
-        args[0] = line;
-        args[1] = NULL;
+		if (cmd_path)
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				if (execve(cmd_path, args, NULL) == -1)
+				{
+					perror(line);
+					exit(EXIT_FAILURE);
+				}
+			}
+			else if (pid > 0)
+			{
+				wait(NULL);
+			}
+			else
+			{
+				perror("fork");
+			}
+			free(cmd_path);
+		}
+		else
+		{
+			printf("./hsh: 1: %s: not found\n", line);
+		}
+	}
 
-        pid = fork();
-        if (pid == 0)
-        {
-            if (execve(line, args, NULL) == -1)
-            {
-                perror("./hsh");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (pid > 0)
-        {
-            wait(NULL);
-        }
-        else
-        {
-            perror("fork");
-        }
-    }
-
-    free(line);
-    return (0);
+	free(line);
+	return (0);
 }
 
