ce2211d8b3dfc8c174836afb62562bf4b68fa2d0
root <pertonsalomee@gmail.com>
2025-01-09 17:07:24 +0000

    multiple changes like delete unused files and making task2 work again

--- main.c
+++ main.c
@@ -11,42 +11,33 @@
 
 int main(void)
 {
-	char *buffer = NULL;
-	size_t buffer_size = 0;
-	int n_char;
-	char *token;
-	char **array;
-	int status, i = 0;
+	char *line = NULL;
+	size_t len = 0;
 	pid_t pid;
 
-	array = malloc(sizeof(char *) * 1024);
 	while (1)
 	{
-		write(1, "$ ", 2);
-		n_char = getline(&buffer, &buffer_size, stdin);
-		if (n_char == -1)
-		{
-			perror("getline");
+		printf("$ ");
+		if (getline(&line, &len, stdin) == -1)
 			break;
-		}
-
-		token = strtok(buffer, "\t\n");
-		while (token)
-		{
-			array[i] = token;
-			token = strtok(NULL, " \t\n");
-			i++;
-		}
-		array[i] = NULL;
+		line[strcspn(line, "\n")] = '\0';
 		pid = fork();
 		if (pid == 0)
 		{
-			if (execve(array[0], array, NULL) == -1)
-				perror("execve");
+			char *args[2];
+			args[0] = line;
+			args[1] = NULL;
+			if (execve(line, args, NULL) == -1)
+			{
+				perror("./hsh");
+				exit(EXIT_FAILURE);
+			}
 		}
-		else
-			wait(&status);
-		i = 0;
-		free(array);
+		else if (pid > 0)
+		{
+			perror("fork");
+		}
 	}
+	free(line);
+	return 0;
 }
