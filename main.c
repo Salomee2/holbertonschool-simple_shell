90c0d94b2d8941acda992219f83aab7a5d94537e
root <pertonsalomee@gmail.com>
2025-01-10 08:27:29 +0000

    trying a mor simple version

--- main.c
+++ main.c
@@ -1,62 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include "shell.h"
 
-int main(void)
+int main()
 {
-	char *line = NULL;
-	size_t len = 0;
-	pid_t pid;
-	char *args[2];
-	char *cmd_path;
+	char *command = NULL;
+	size_t bufsize = 0;
+	printf("$ ");
 
-	while (1)
+	if (getline(&command, &bufsize, stdin) == -1)
 	{
-		printf("$ ");
-		if (getline(&line, &len, stdin) == -1)
-		{
-			printf("\n");
-			break;
-		}
-
-		line[strcspn(line, "\n")] = '\0';
-
-		if (strlen(line) == 0)
-			continue;
-
-		cmd_path = find_command_in_path(line);
-
-		if (cmd_path)
-		{
-			pid = fork();
-			if (pid == 0)
-			{
-				if (execve(cmd_path, args, NULL) == -1)
-				{
-					perror(line);
-					exit(EXIT_FAILURE);
-				}
-			}
-			else if (pid > 0)
-			{
-				wait(NULL);
-			}
-			else
-			{
-				perror("fork");
-			}
-			free(cmd_path);
-		}
-		else
-		{
-			printf("./hsh: 1: %s: not found\n", line);
-		}
+		perror("Error reading command");
+		exit(EXIT_FAILURE);
 	}
-
-	free(line);
-	return (0);
+	printf("You entered: %s", command);
+	free(command);
+	return 0;
 }
-
